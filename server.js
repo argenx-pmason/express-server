5b410aa4b4077ff248b5bce0d82ecee89225457e
philipmason <phil@woodstreet.org.uk>
2023-05-25 20:31:23 +0100

    enhancements

--- server.js
+++ server.js
@@ -4,20 +4,9 @@ const express = require("express"),
   // cors = require("cors"),
   PORT = process.env.PORT || 3001,
   app = express(),
-  // csvToJson = require("convert-csv-to-json"),
-  // papa = require("papaparse"),
-  // csv = require("csvtojson"),
-  // nodemailer = require("nodemailer"),
-  // mail = require("nodemailer").mail,
-  // sqlite3 = require("sqlite3").verbose(),
-  // make a reference to each database
-  // sdtm = new sqlite3.Database("./data/sdtm.db"),
-  // sdtm2 = new sqlite3.Database("./data/sdtm2.db"),
-  // sdtm4 = new sqlite3.Database("./data/sdtm4.db"),
-  // global = new sqlite3.Database("./data/global.db"),
-  // urlNotes = new sqlite3.Database("./data/url-notes.db"),
-  // chatController = require("./chatController"),
-  fs = require("fs");
+  fs = require("fs"),
+  nodemailer = require("nodemailer"),
+  http2 = require("http2");
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -37,16 +26,14 @@ app.get("/test-server", (req, res) => {
 
 // http://localhost:3001/dir/., http://localhost:3001/dir/..%2F..%2F.., http://localhost:3001/dir/..%2F..%2F..%2fusers%2fpmason
 app.get("/dir/:folder", (req, res) => {
-  const fs = require("fs"),
-    folder = req.params.folder,
-    path = "./" + folder + "/",
-    files = fs.readdirSync(path);
-  console.log("folder = ", folder, "path = ", path, "files = ", files);
+  const folder = req.params.folder,
+    files = fs.readdirSync(folder);
+  console.log("folder = ", folder, "files = ", files);
   res.send(files);
 });
 
 // e.g. http://localhost:3001/getfile/server.js, http://localhost:3001/getfile/..%2Fpackage.json, http://localhost:3001/getfile/..%2F..%2FTemp%2Fsetenv.log
-app.get("/getfile/:file", (req, res) => {
+app.get("/getcsv/:file", (req, res) => {
   // name of file to as a query parameter to return contents of file
   const file = req.params.file;
   console.log("/getfile: ", file);
@@ -60,6 +47,82 @@ app.get("/getfile/:file", (req, res) => {
       res.status(200).send(fileData);
     }
   });
+});
+
+app.get("/getfile/:dirName/:fileName", (req, res) => {
+  const { fileName, dirName } = req.params;
+  const filePath = `${dirName}/${fileName}`;
+  const jsonContent = fs.readFileSync(filePath, {
+    encoding: "utf8",
+    flag: "r",
+  });
+  res.header("Content-Type", "text/json");
+  res.status(200).send(jsonContent);
+});
+
+app.get("/gmail", (req, res) => {
+  // create reusable transport method (opens pool of SMTP connections)
+  const smtpTransport = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "xploratum@gmail.com",
+        pass: "ynmngtxxcyvtqzmj",
+      },
+    }),
+    // setup e-mail data with unicode symbols
+    mailOptions = {
+      from: "Fred Foo ✔ <phil@woodstreet.org.uk>", // sender address
+      to: "test@woodstreet.org.uk", // list of receivers
+      subject: "Hello", // Subject line
+      text: "Hello world", // plaintext body
+      html: "<b>Hello world ✔</b>", // html body
+    };
+
+  // send mail with defined transport object
+  smtpTransport.sendMail(mailOptions, function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Message sent: " + response.message);
+    }
+
+    // if you don't want to use this transport object anymore, uncomment following line
+    //smtpTransport.close(); // shut down the connection pool, no more messages
+  });
+});
+
+app.get("/email", async (req, res) => {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+  // console.log(testAccount);
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+  // console.log(transporter);
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Phil Mason 👻" <phil@woodstreet.org.uk>', // sender address
+    to: "phil@woodstreet.org.uk", // list of receivers
+    subject: "Test Email", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+  // console.log(info);
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  res.status(200).send(nodemailer.getTestMessageUrl(info));
 });
 
 app.listen(PORT, () => {
